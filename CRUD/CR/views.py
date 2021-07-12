467e9c3cf109762e6b6791c9b17d7990e7edb8be
ahmedyounis253 <amy2532001@gmail.com>
2021-07-12 03:13:38 +0200

    first commit

---
+++ CRUD/CR/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,redirect
+from django. http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+
+from .forms import loginForm
+# Create your views here.
+
+def main(request):
+    return render(request,'main.html')
+def loginView(request):
+    if request.method=="POST":
+    
+        data=request.POST
+        form = loginForm(data)
+
+        if form.is_valid():
+            username=data['username']
+            password=data['password']
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('/')
+            
+        
+        return render(request,'login.html',{'error':True,'form':form})
+    else :
+        form=loginForm()
+        print(form.as_table())
+        return render(request,'login.html',{'error':False,'form':form})
+
+def logoutView(request):
+    logout(request)
+    return redirect('/')
